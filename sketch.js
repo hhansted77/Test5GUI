cb81620bd9cceb7954d31b5dbd9c686b62952aa9
Hannah Hansted <hmh22@fsu.edu>
2024-09-22 19:30:28 -0400

    video tutorial

--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
+let d = 60;
+let deg = 58;
+let outline = true
+
+let gui;
+let diameterSlider, degSlider, outlineCheckbox;
+
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  strokeWeight(10);
+
+  gui = createGui();
+  diameterSlider = createSlider("Diameter", 200, 10, 100, 20, 10, 100);
+  degSlider = createSlider("Rotation", 200, 40, 100, 20, 0, 180);
+  degSlider.val = 23;
+  outlineCheckbox = createCheckbox("Outline", 340, 10, 20,20, true);
 }
 
 function draw() {
   background(125);
+  drawGui();
+
+  if(outlineCheckbox.val){
+    stroke("black");
+  } else {
+    noStroke();
+  }
+
+
+
+
+ellipse(100, 200, diameterSlider.val,);
+
+push();
+translate(200, 200);
+rotate( radians(degSlider.val) );
+rect(0, 0, 60, 60);
+pop();
+
+noStroke();
+text(diameterSlider.label, 140, 20);
+
 }
