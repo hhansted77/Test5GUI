32c31649f59a139f79b0a6c24571df3907ea9d37
Hannah Hansted <hmh22@fsu.edu>
2024-09-22 21:58:26 -0400

    some customizing

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let d = 60;
 let deg = 58;
-let outline = true
+let outline = true;
 
 let gui;
 let diameterSlider, degSlider, outlineCheckbox;
@@ -11,18 +11,32 @@ function setup() {
   strokeWeight(10);
 
   gui = createGui();
-  diameterSlider = createSlider("Diameter", 200, 10, 100, 20, 10, 100);
-  degSlider = createSlider("Rotation", 200, 40, 100, 20, 0, 180);
+  diameterSlider = createSlider("Diameter", 40, 330, 100, 20, 10, 100);
+  //diameterSlider.onChange = diameterSliderChange;
+
+diameterSlider.setStyle({
+  fillBg: color(127, 153, 104),
+  fillBgHover: color(208, 235, 185),
+})
+
+  degSlider = createSlider("Rotation", 250, 330, 100, 20, 0, 180);
   degSlider.val = 23;
-  outlineCheckbox = createCheckbox("Outline", 340, 10, 20,20, true);
+
+degSlider.setStyle({
+  fillBg: color(127, 153, 104),
+  fillBgHover: color(208, 235, 185),
+})
+
+  outlineCheckbox = createCheckbox("Outline", 183, 330, 20,20, true);
 }
 
 function draw() {
-  background(125);
+  background(177, 209, 148);
   drawGui();
 
+
   if(outlineCheckbox.val){
-    stroke("black");
+    stroke(59, 37, 25);
   } else {
     noStroke();
   }
@@ -30,15 +44,21 @@ function draw() {
 
 
 
-ellipse(100, 200, diameterSlider.val,);
+ellipse(90, 200, diameterSlider.val);
 
 push();
-translate(200, 200);
+translate(300, 200);
 rotate( radians(degSlider.val) );
 rect(0, 0, 60, 60);
 pop();
 
 noStroke();
-text(diameterSlider.label, 140, 20);
+textSize(15)
+text(diameterSlider.label, 55, 320);
+text(degSlider.label, 270, 320)
 
 }
+
+//function diameterSliderChange();{
+ //when i tried to do this part of the video, for some reason it breaks eveyrthing
+//}
